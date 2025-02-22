925861ed32961042b1f756e00255f738449744d3
WHitE-TITaN <dk488621@gmail.com>
2025-02-22 15:41:04 +0530

    added dynamic input graph

--- sorting-visualizer/src/components/Graph.js
+++ sorting-visualizer/src/components/Graph.js
@@ -1,39 +1,57 @@
-import React from 'react'
+import React, { Component } from 'react'
 
 
-function readInput(bar){
-    let arrayString = document.getElementById('userInput').value;
-    bar = arrayString.split(',').map(num => Number(num.trim()));
-    
-    console.log(bar);
-}
 
-function Graph() {
-    const bar = [90, 10, 50, 70, 80, 77, 30, 45];
 
-    return (
-        <div className='mainInput'>
-            <input id='userInput' placeholder='enter array'></input>
-            <button onClick={() => readInput(bar)}>submmit</button>
+class Graph extends Component {
 
-            <div className='MainGraph'>
-                {
-                    bar.map((Hight, index) => (<div
+    constructor(prop) {
+        super(prop)
+        this.state = {
+            bar: [90, 10, 50, 70, 80, 77, 30, 45]
+        };
+    }
 
-                        key={index}
-                        style={{
-                            height: `${Hight}px`,
-                            width: `40px`,
-                            
-                            borderRadius: `5px`,
-                            backgroundColor: 'greenyellow'
-                        }}>
+    readInput() {
+        let arrayString = document.getElementById('userInput').value;
+        let newBar = arrayString.split(',').map(num => Number(num.trim()));
 
-                    </div>))
-                }
+        this.setState({ bar: newBar });
+        console.log(newBar);
+    }
+
+    resetGraph(){
+        const defaultState = [100, 100, 100, 100, 100, 100, 100];
+        this.setState({bar: defaultState});
+    }
+
+    render() {
+        return (
+            <div className='mainInput'>
+                <input id='userInput' placeholder='enter array'></input>
+                <button onClick={() => this.readInput()}>submmit</button>
+                <button onClick={() => this.resetGraph()}>Reset</button>
+
+                <div className='MainGraph'>
+                    {
+                        this.state.bar.map((Hight, index) => (<div
+
+                            key={index}
+                            style={{
+                                height: `${Hight}px`,
+                                width: `40px`,
+
+                                borderRadius: `5px`,
+                                backgroundColor: 'greenyellow'
+                            }}>
+
+                        </div>))
+                    }
+                </div>
             </div>
-        </div>
-    )
+        )
+    }
+
 }
 
 
