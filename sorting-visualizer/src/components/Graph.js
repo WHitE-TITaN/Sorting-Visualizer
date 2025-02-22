0c0cd72885cdf5452ee344b9783b457cc66f1d74
WHitE-TITaN <dk488621@gmail.com>
2025-02-22 14:15:26 +0530

    added multi element rendreng at once

--- sorting-visualizer/src/components/Graph.js
+++ sorting-visualizer/src/components/Graph.js
@@ -1,40 +1,42 @@
-import React, {Component} from 'react'
+import React from 'react'
 
-class Graph extends Component{
 
-    constructor (props){
-        super (props)
-
-        this.state = {
-            message : "Welcome !"
-        }
-    }
-
-    HI(){
-        this.setState({
-            message: "Hi"
-        })
-    }
-
-    readInput(){
-        let arrayString = document.getElementById('userInput').value; // Get input as string
-        let newArray = arrayString.split(',').map(num => Number(num.trim())); // Convert to array of numbers
-
-        console.log(newArray); // Log the entire array
-        newArray.forEach(element => console.log(element));
-    }
-
-    render(){
-        return(
-            <div className='MainGraph'>
-                <input id='userInput' placeholder='enter array'></input>
-                <button onClick={() => this.readInput()}>submmit</button>
-                <h1>{this.state.message}</h1>
-                <button onClick={() => this.HI()}>HI</button>
-            </div>
-            
-        )
-    }
+function readInput(){
+    let arrayString = document.getElementById('userInput').value;
+    let newArray = arrayString.split(',').map(num => Number(num.trim()));
+    
+    console.log(newArray);
+    newArray.forEach(element => console.log(element));
 }
+
+function Graph() {
+    const bar = [90, 10, 50, 70, 80, 77, 30, 45];
+
+    return (
+        <div className='mainInput'>
+            <input id='userInput' placeholder='enter array'></input>
+            <button onClick={() => readInput()}>submmit</button>
+
+            <div className='MainGraph'>
+                {
+                    bar.map((Hight, index) => (<div
+
+                        key={index}
+                        style={{
+                            height: `${Hight}px`,
+                            width: `40px`,
+                            
+                            borderRadius: `5px`,
+                            backgroundColor: `greenyellow`
+                        }}>
+
+                    </div>))
+                }
+            </div>
+        </div>
+    )
+}
+
+
 
 export default Graph
